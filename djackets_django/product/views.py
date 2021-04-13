1229a3948c610ea32ffccc267b3f5045a137cf90
Max Blacher-L <m.blacher@web.de>
2021-04-13 17:53:46 +0200

    create product backend

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -11,3 +11,18 @@ class LatestProductsList(APIView):
         products = Product.objects.all()[:4]
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+
+        return Response(serializer.data)
+
+        
